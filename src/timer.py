615f809e64843a7768834fec3b7a2ec665cf0407
Adriel Arsenault <adriel.arsenault@gmail.com>
2017-07-11 22:44:03 -0400

    display function shows minutes

--- src/timer.py
+++ src/timer.py
@@ -2,83 +2,89 @@ import os
 import time
 
 
-
-
 def clear():
     os.system('clear')  # on linux / os x
 
+HEIGHT = 5
 
 NUMBERS = {
 
-1:'''\
-  ██
-████
-  ██
-  ██
+'1':'''\
+  ██  
+████  
+  ██  
+  ██  
 ██████
 ''',
-2:'''\
- ████
+'2':'''\
+ ████ 
 █   ██
-   ██
- ██
-██████  
+   ██ 
+ ██   
+██████
 ''',
-3:'''\
- ████
-█   ██
-   ██
-█   ██
- ████  
+'3':'''\
+ ████ 
+█    █
+   ██ 
+█    █
+ ████ 
 ''',
-4:'''\
-  ███   
- █ ██
-█  ██
+'4':'''\
+  ███ 
+ █ ██ 
+█  ██ 
 ██████
    ██ 
 ''',
-5:'''\
+'5':'''\
 ██████
-█
+█     
 ██████
      █
-██████  
-''',
-6:'''\
 ██████
-█  
+''',
+'6':'''\
+██████
+█     
 ██████
 █    █
-██████  
+██████
 ''',
-7:'''\
+'7':'''\
 ██████
 █   ██
-   ██
-  ██
- ██  
+   ██ 
+  ██  
+ ██   
 ''',
-8:'''\
- ████
+'8':'''\
+ ████ 
 █    █
 ██████
 █    █
- ████  
+ ████ 
 ''',
-9:'''\
+'9':'''\
 ██████
 █    █
 ██████
     ██
-    ██  
+    ██
 ''',
-0:'''\
- ████
+'0':'''\
+ ████ 
 █    █
 █    █
 █    █
- ████  
+ ████ 
+''',
+":":'''\
+      
+  ██  
+      
+  ██  
+      
 '''}
 
 def test():
@@ -88,6 +94,63 @@ def test():
         time.sleep(1)
         clear()
 
-test()
+        #TODO: supress cursor
+# test()
+
+# input in the form of hours and minutes
+def vertical_display(time):
+    for char in time:
+        print(NUMBERS[char])
+
+
+def display_time(time):
+
+    # break down each letter into individual lines
+    letter_lines = []
+    for char in time:
+
+        big_letter = NUMBERS[char]
+        letter_lines.append(big_letter.split('\n'))
+    #
+    # # create scan line
+    # composed_lines = [[]]*HEIGHT
+    #
+    # # print(letter_lines)
+    # print(composed_lines)
+
+    # Loops over the lines
+    for n in range(HEIGHT):
+        # Loops over the letters
+        for letter in letter_lines:
+            # takes the nth line of the letter
+            print(letter[n], end=' ')
+        print(' ')
+
+    # for line in composed_lines:
+    #     print(''.join(line))
+
+
+def countdown(time):
+
+    # for now assume time is minutes
+
+
+    # convert it to a sting
+    time = str(time)
+
+    if len(time)
+
+    if len(time) == 2:
+        time = time + ':00'
+
+    # counter
+    digits = time.split(':')
+
+    ld = len(digits)
+
+    if ld > 0:
+        sec
+
+
 
 
